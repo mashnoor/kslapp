53f517974d1470839e5a40506380f7d55c498e38
Nowfel Mashnoor <nmmashnoor@gmail.com>
2017-08-14 21:39:50 +0600

    Changed Library to retrofit

---
+++ app/src/main/java/com/xtremebd/ksl/clients/ApiClient.java
@@ -0,0 +1,32 @@
+package com.xtremebd.ksl.clients;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Mashnoor on 8/14/17.
+ */
+
+public class ApiClient {
+    private static final String BASE_URL = "http://128.199.157.233"; //address of your localhost
+    private static Retrofit retrofit = null;
+
+    private static Gson gson = new GsonBuilder()
+            .setLenient()
+            .create();
+
+    private ApiClient() {} // So that nobody can create an object with constructor
+
+    public static synchronized Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return retrofit;
+    }
+}
---
+++ app/src/main/java/com/xtremebd/ksl/interfaces/ApiInterface.java
@@ -0,0 +1,21 @@
+package com.xtremebd.ksl.interfaces;
+
+import com.xtremebd.ksl.models.Item;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+
+/**
+ * Created by Mashnoor on 8/14/17.
+ */
+
+public interface ApiInterface {
+
+    @GET("/all_items_latest_update.txt")
+    Call<List<Item>> getAllLatestItemUpdates();
+
+
+}
